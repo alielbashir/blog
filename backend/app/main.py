565a656081bdae6e9d78a726dfe107042259eea6
Ali Elbashir <alielbashiir@gmail.com>
2022-04-16 07:44:39 +0300

    feat: add mongo initialization on startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
+from models.user import User
+from config import CONFIG
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def init_app():
+    app.db = AsyncIOMotorClient(CONFIG.mongo_uri).account
+    await init_beanie(app.db, document_models=[User])
 
 
 @app.get("/")
