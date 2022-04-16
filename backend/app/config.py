3817a210679dda5469324a5b41d7b93c17ac0c80
Ali Elbashir <alielbashiir@gmail.com>
2022-04-16 06:43:26 +0300

    Add config file

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+from decouple import config
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    """Server config settings"""
+
+    # Mongo Engine settings
+    mongo_uri = config("MONGO_URI")
+
+    # Security settings
+    authjwt_secret_key = config("SECRET_KEY")
+    salt = config("SALT").encode()
+
+    testing = config("TESTING", default=False, cast=bool)
+
+
+CONFIG = Settings()
