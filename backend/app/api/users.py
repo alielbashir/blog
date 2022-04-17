88a6502dc6403ffb9bd790cbef8e9f2f6335efd5
Ali Elbashir <alielbashiir@gmail.com>
2022-04-17 06:12:23 +0300

    feat: implement login

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -21,3 +21,16 @@ async def register(user_auth: UserAuth):
     await user.create()
 
     return user
+
+
+@router.post("/login")
+async def login(user_auth: UserAuth):
+    user = await User.find_one(User.username == user_auth.username)
+
+    if user is None or not auth_handler.verify_password(
+        user_auth.password, user.password
+    ):
+        raise HTTPException(status_code=401, detail="Invalid username and/or password")
+    token = auth_handler.encode_token(user.username)
+
+    return {"token": token}
