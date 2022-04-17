3e3530753a98754550d85fd1eeaa06abb2d756a9
Ali Elbashir <alielbashiir@gmail.com>
2022-04-17 06:42:49 +0300

    chore: add back commented new user test

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -10,16 +10,16 @@ async def test_root(client: AsyncClient) -> None:
     assert data["message"] == "Hello World"
 
 
-# @pytest.mark.asyncio
-# async def test_new_user(client: AsyncClient) -> None:
-#     new_user = {"username": "jakeperalta", "password": "amysantiago"}
-#     response = await client.post("/users/register", json=new_user)
-#     assert response.status_code == 201
-#     data = response.json()
-#     assert data["username"] == "jakeperalta"
+@pytest.mark.asyncio
+async def test_new_user(client: AsyncClient) -> None:
+    new_user = {"username": "jakeperalta", "password": "amysantiago"}
+    response = await client.post("/users/register", json=new_user)
+    assert response.status_code == 201
+    data = response.json()
+    assert data["username"] == "jakeperalta"
 
-#     # check if user added to db
-#     response = await client.post("/users/login", json=new_user)
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert "token" in data
+    # check if user added to db
+    response = await client.post("/users/login", json=new_user)
+    assert response.status_code == 200
+    data = response.json()
+    assert "token" in data
