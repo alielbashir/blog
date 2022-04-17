815022ad3a5c7f5d3bee3c681eb0c1e00b15ee3c
Ali Elbashir <alielbashiir@gmail.com>
2022-04-17 05:05:19 +0300

    feat: implement user registration

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from core.auth import AuthHandler
 
+from models.user import User, UserAuth, UserOut
+
+auth_handler = AuthHandler()
 
 router = APIRouter(prefix="/users")
 
-@router.post
-async def register()
+
+@router.post("/register", status_code=201, response_model=UserOut)
+async def register(user_auth: UserAuth):
+    # TODO: #5 handle different privileges
+    user = await User.find_one(User.username == user_auth.username)
+    if user is not None:
+        raise HTTPException(status_code=400, detail="Username is taken")
+    hashed_pass = auth_handler.get_password_hash(user_auth.password)
+
+    # replace plaintext pass with hashed pass
+    user = User(username=user_auth.username, password=hashed_pass)
+    await user.create()
+
+    return user
