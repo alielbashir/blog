475fb6b8d540475527891ec22a3cd22c4388ba91
Ali Elbashir <alielbashiir@gmail.com>
2022-04-22 04:52:06 +0300

    feat: add user models to handle scope

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,16 +1,29 @@
+from enum import Enum
 from beanie import Document
 from pydantic import BaseModel
+
+
+class Scope(str, Enum):
+    """Sope to control what a user is authorized to do"""
+    read = "read"
+    write = "write"
+
 
 
 class UserAuth(BaseModel):
     username: str
     password: str
 
+class UserRegister(UserAuth):
+    scope: Scope = Scope.read
+
 
 class User(Document):
     username: str
     password: str
+    scope: Scope
 
 
 class UserOut(BaseModel):
     username: str
+    scope: Scope
