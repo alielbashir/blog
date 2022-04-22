72e4f5a55173f1029e3212956fdf696d303dbe24
Ali Elbashir <alielbashiir@gmail.com>
2022-04-22 05:32:28 +0300

    chore: change test name to be more descriptive

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -13,7 +13,7 @@ async def test_root(client: AsyncClient) -> None:
 
 
 @pytest.mark.asyncio
-async def test_new_user(client: AsyncClient) -> None:
+async def test_new_user_login(client: AsyncClient) -> None:
     new_user = {"username": "jakeperalta", "password": "amysantiago"}
     response = await client.post("/users/register", json=new_user)
     assert response.status_code == 201
