0f28033ef5c831da0415daa6ad4d2f659203a95b
Ali Elbashir <alielbashiir@gmail.com>
2022-04-22 18:59:03 +0300

    feat: Add user scope registration (#9)
    * feat: add user models to handle scope
    
    * chore: add scope test for registration
    
    * chore: change test name to be more descriptive
    
    * feat: add scopes for users
    
    * chore: format with black

--- backend/src/api/users.py
+++ backend/src/api/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from src.auth import AuthHandler
 
-from src.models.user import User, UserAuth, UserOut
+from src.models.user import User, UserAuth, UserOut, UserRegister
 
 auth_handler = AuthHandler()
 
@@ -9,15 +9,16 @@ router = APIRouter(prefix="/users")
 
 
 @router.post("/register", status_code=201, response_model=UserOut)
-async def register(user_auth: UserAuth):
-    # TODO: #5 handle different privileges
-    user = await User.find_one(User.username == user_auth.username)
+async def register(user_register: UserRegister):
+    user = await User.find_one(User.username == user_register.username)
     if user is not None:
         raise HTTPException(status_code=400, detail="Username is taken")
-    hashed_pass = auth_handler.get_password_hash(user_auth.password)
+    hashed_pass = auth_handler.get_password_hash(user_register.password)
 
     # replace plaintext pass with hashed pass
-    user = User(username=user_auth.username, password=hashed_pass)
+    user = User(
+        username=user_register.username, password=hashed_pass, scope=user_register.scope
+    )
     await user.create()
 
     return user
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,6 +1,8 @@
 from httpx import AsyncClient
 import pytest
 
+from src.models.user import Scope
+
 
 @pytest.mark.asyncio
 async def test_root(client: AsyncClient) -> None:
@@ -11,7 +13,7 @@ async def test_root(client: AsyncClient) -> None:
 
 
 @pytest.mark.asyncio
-async def test_new_user(client: AsyncClient) -> None:
+async def test_new_user_login(client: AsyncClient) -> None:
     new_user = {"username": "jakeperalta", "password": "amysantiago"}
     response = await client.post("/users/register", json=new_user)
     assert response.status_code == 201
@@ -23,3 +25,29 @@ async def test_new_user(client: AsyncClient) -> None:
     assert response.status_code == 200
     data = response.json()
     assert "token" in data
+
+
+@pytest.mark.asyncio
+async def test_new_user_with_default_scope(client: AsyncClient) -> None:
+    """checks that scope defaults to read when not specified"""
+    new_user = {"username": "terry", "password": "oldspice"}
+    response = await client.post("/users/register", json=new_user)
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["username"] == "terry"
+    assert data["scope"] == Scope.read
+
+
+@pytest.mark.asyncio
+async def test_new_user_with_write_scope(client: AsyncClient) -> None:
+    """checks that new user's scope is write when specified"""
+    new_user = {"username": "captainholt", "password": "cheddar", "scope": "write"}
+    response = await client.post("/users/register", json=new_user)
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["username"] == "captainholt"
+    assert data["scope"] == Scope.write
