708d1660badf46840f1b0b1dad46e99f09b6871f
Ali Elbashir <alielbashiir@gmail.com>
2022-05-25 18:30:19 +0300

    refactor: use session storage for state

--- frontend/src/stores/posts.js
+++ frontend/src/stores/posts.js
@@ -1,21 +1,28 @@
+import { useSessionStorage } from "@vueuse/core";
 import axios from "axios";
 import { defineStore } from "pinia";
+import { useUsersStore } from "./users";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const usePostsStore = defineStore("posts", {
   state: () => ({
-    posts: [],
+    posts: useSessionStorage("posts", []),
   }),
   actions: {
     async getPosts() {
+      const { token } = useUsersStore();
       const { data } = await axios.get(`${API_URL}/posts`, {
         headers: {
-          Authorization: "Bearer " + sessionStorage.token,
+          Authorization: "Bearer " + token,
         },
       });
       this.posts = data;
     },
     async createPost(title, content) {
+      console.log(`title = ${title}`);
+      console.log(`content = ${content}`);
+
+      const { token } = useUsersStore();
       const { newPost } = await axios.post(
         `${API_URL}/posts`,
         {
@@ -24,7 +31,7 @@ export const usePostsStore = defineStore("posts", {
         },
         {
           headers: {
-            Authorization: "Bearer " + sessionStorage.token,
+            Authorization: "Bearer " + token,
           },
         }
       );
