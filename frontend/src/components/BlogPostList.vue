945a52c28911e3dee943477adffcdebfd912485e
Ali Elbashir <alielbashiir@gmail.com>
2022-05-25 23:37:07 +0300

    feat: add sorting by date and votes

--- frontend/src/components/BlogPostList.vue
+++ frontend/src/components/BlogPostList.vue
@@ -2,25 +2,83 @@
 import { storeToRefs } from "pinia";
 import { usePostsStore } from "../stores/posts";
 import BlogPost from "../components/BlogPost.vue";
-import { watch } from "vue";
+import { watch, ref } from "vue";
 import { computed } from "@vue/reactivity";
 
 const store = usePostsStore();
 
-const { updatingToggle } = storeToRefs(store);
+const { updatingToggle, posts } = storeToRefs(store);
 
 const { fetchPosts } = store;
+
+const sortModel = ref("date");
+const sortOrderModel = ref("desc");
+
 const dateSortedPosts = computed(() => store.dateSortedPosts);
 const voteSortedPosts = computed(() => store.voteSortedPosts);
 
+watch([sortModel, sortOrderModel], () => {
+  switch (sortModel.value) {
+    case "votes":
+      posts.value =
+        sortOrderModel.value === "desc"
+          ? voteSortedPosts.value
+          : [...voteSortedPosts.value].reverse();
+      break;
+    case "date":
+      posts.value =
+        sortOrderModel.value === "desc"
+          ? dateSortedPosts.value
+          : [...dateSortedPosts.value].reverse();
+      break;
+    default:
+      break;
+  }
+});
 watch(updatingToggle, fetchPosts);
 fetchPosts();
 </script>
 
 <template>
-  <div>
-    <div v-for="post in dateSortedPosts" :key="post.id">
+  <section>
+    <br />
+    <label>Sort by:</label><br />
+    <label
+      ><input
+        checked="checked"
+        name="sorting"
+        type="radio"
+        value="date"
+        v-model="sortModel"
+      />
+      Date</label
+    >
+    <br />
+    <label
+      ><input name="sorting" type="radio" value="votes" v-model="sortModel" />
+      Votes</label
+    >
+    <br />
+    <br />
+    <label>Order: </label><br />
+    <label
+      ><input
+        checked="checked"
+        name="order"
+        type="radio"
+        value="asc"
+        v-model="sortOrderModel"
+      />
+      Ascending</label
+    >
+    <br />
+    <label
+      ><input name="order" type="radio" value="desc" v-model="sortOrderModel" />
+      Descending</label
+    >
+    <br />
+    <div v-for="post in posts" :key="post.id">
       <BlogPost :post="post" />
     </div>
-  </div>
+  </section>
 </template>
