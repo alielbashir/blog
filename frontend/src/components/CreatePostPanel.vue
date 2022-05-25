f14d33b357f2bb4beb9a72eab62a5c61972b2d1a
Ali Elbashir <alielbashiir@gmail.com>
2022-05-25 17:53:07 +0300

    feat: add conditional rendering based on scope

---
+++ frontend/src/components/CreatePostPanel.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const titleModel = ref("");
+const contentModel = ref("");
+</script>
+
+<template>
+  <p>
+    <label>What's happening?</label> <br />
+    <input type="text" placeholder="Title" v-model="titleModel" />
+    <textarea rows="6" placeholder="Content" v-model="contentModel"></textarea>
+  </p>
+</template>
